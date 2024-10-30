c7ae62303db466ff87960106e9f5d7157ec0f137
Emma Tong <53305901+etong11@users.noreply.github.com>
2024-10-30 19:12:48 -0400

    Create AppView and navbar tabs directing to other views

---
+++ Team10Firebase/Views/Main Tab Views/ScanView.swift
@@ -0,0 +1,20 @@
+//
+//  ScanView.swift
+//  Team10Firebase
+//
+//  Created by Emma Tong on 10/30/24.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    var body: some View {
+      VStack {
+        Text("Scan")
+      }
+    }
+}
+
+#Preview {
+    ScanView()
+}
---
+++ Team10Firebase/Views/TextParserView.swift
@@ -0,0 +1,18 @@
+//
+//  TextParserView.swift
+//  Team10Firebase
+//
+//  Created by Emma Tong on 10/30/24.
+//
+
+import SwiftUI
+
+struct TextParserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextParserView()
+}
