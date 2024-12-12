4b52eb55960ab0856df572839c4439314d119603
Emma Tong <53305901+etong11@users.noreply.github.com>
2024-12-12 00:58:27 -0500

    Resolving missing code from merge conflict

--- Team10Firebase/Views/TextParserView.swift
+++ Team10Firebase/Views/TextParserView.swift
@@ -257,7 +257,6 @@ struct TextParserView: View {
             dispatchGroup.enter()
             openAI.parseImage(image) { text in
                 if let text = text {
-                    // parsedTexts[index] = text
                     let strippedText = text.trimmingCharacters(in: CharacterSet(charactersIn: "`"))
                     parsedTexts[index] = strippedText
                 } else {
@@ -310,7 +309,7 @@ struct TextParserView: View {
                             .padding()
                     } else {
                         ScrollView {
-                            Text(text)
+                            Text(.init(text))
                                 .frame(maxWidth: .infinity, alignment: .leading)
                                 .padding()
                         }
